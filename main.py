169882b895b068eb7b0ad15a44bfc6134c6ba085
Jason Sohl <jason@frozensoliddesigns.com>
2021-08-21 02:00:11 -0700

    adding a lot of things, making the scan work more or less

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import requests
 import os
 import sys
+import hashlib
 
 virustotal_uri = "https://virusshare.com/hashfiles/VirusShare_"
-existing_hashes = []
+hash_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)),'existing_hashes')
+infected_hashes = []
 
-if __name__ == "__main__":
+def update_hashes():
+  if not os.path.isdir(hash_directory):
+    os.mkdir(hash_directory)
   
-  directory = "existing_hashes"
-  path = os.path.join(os.path.dirname(os.path.realpath(__file__)),directory)
-
-  if not os.path.isdir(path):
-    os.mkdir(path)
-  
-  existing_hashes = os.listdir(path)
+  existing_hashes = os.listdir(hash_directory)
   existing_hashes = [f.lower() for f in existing_hashes]
   existing_hashes = sorted(existing_hashes)
 
@@ -25,7 +23,7 @@ if __name__ == "__main__":
     print(f'Status Code: {r.status_code}')
 
     if r.status_code == 200:
-      with open(path+'/hash_00000.md5', 'wb') as f:
+      with open(hash_directory+'/hash_00000.md5', 'wb') as f:
         f.write(r.content)
 
   last_hashfile = existing_hashes[-1]
@@ -40,9 +38,75 @@ if __name__ == "__main__":
     r = requests.get(url)
 
     if r.status_code == 200:
-      with open(path+'/hash_'+next_hash+'.md5', 'wb') as f:
+      with open(hash_directory+'/hash_'+next_hash+'.md5', 'wb') as f:
         f.write(r.content)
       print(f'Downloaded {next_hash}.')
     else:
       print('Hashes up-to-date.')
       break
+
+def compare_hash(hash):
+  match = False
+  existing_hashes = os.listdir(hash_directory)
+  print(f'Checking hash {hash}')
+
+  file_counter = 1
+  for file in existing_hashes:
+    with open(hash_directory+'/'+file) as lines:
+      for line in lines:
+        line = line.strip()
+        print(f'({round((file_counter/len(existing_hashes))*100)}%) {line}', end='\r')
+        if line == hash:
+          match = True
+          print(f'Found one: {hash}')
+          infected_hashes.append(hash)
+          break
+      else:
+        file_counter += 1
+        continue
+      break
+
+  if not match:
+    print(f'{hash} Clean!')
+
+def compare_hashes(hashed_files):
+  counter = 1
+  for hash in hashed_files:
+    print(f'Hash {counter}/{len(hashed_files)}...')
+    compare_hash(hash)
+    counter += 1
+
+def scan_directory(directory_to_scan):
+  print(f'Scanning Directory: {directory_to_scan}...')
+  files_to_hash = os.listdir(directory_to_scan)
+  hashed_files = []
+
+  for file in files_to_hash:
+    filename = directory_to_scan+'/'+file
+    hashed_file = hashlib.md5(open(filename,'rb').read()).hexdigest()
+    hashed_files.append(hashed_file)
+  compare_hashes(hashed_files)
+
+if __name__ == "__main__":
+  
+  try:
+    update_hashes()
+
+    directory_to_scan = os.path.realpath('/home/jason/Documents')
+
+    scan_directory(directory_to_scan)
+
+    print('Scan Complete.')
+    if len(infected_hashes) > 0:
+      print('Infections found:')
+      for hash in infected_hashes:
+        print(f'Hash: {hash}')
+
+  except KeyboardInterrupt:
+    print("")
+    print('Scan Interrupted.')
+    if len(infected_hashes) > 0:
+      print('Infections found:')
+      for hash in infected_hashes:
+        print(f'Hash: {hash}')
+    sys.exit(0)
